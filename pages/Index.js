74059867e74919cc94f88929d1de6fb66df624bb
jessé <jessemarques.oliveira91@gmail.com>
2025-10-08 20:20:23 -0300

    update index.js
    add nome, titulo, ano, generom[, nota e img

--- pages/Index.js
+++ pages/Index.js
@@ -1,8 +1,38 @@
-function home () {
-  return <> <h2> FILMES </h2>
 
-<p>LISTAGEM DE FILMES </p>
+import { filmes } from "../data/filmes.js";
+
+
+
+function home () {
+  console.log(filmes)
+
+  const primeirofilme = filmes[0]
+
+  const estiloimagem={
+       width:"300px"
+
+  };
+
+return <> <h2> FILMES </h2>
+
+   <ul>
+
+        <li>
+
+              <p> NOME DO FILMEs: </p>
+              <p> {primeirofilme.titulo} </p>
+              <p>ano: {primeirofilme.ano}</p>
+              <p>gênero: {primeirofilme.genero}</p>
+              <p> nota: {primeirofilme.nota}</p>
+              
+              <img style={estiloimagem} src={primeirofilme.poster}></img>
+
+
+        </li>
+
+
+   </ul>
 </>
 
 }
-export default home;
+export default home; 
